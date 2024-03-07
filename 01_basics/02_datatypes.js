5beaa51ec915f5db859bb590b966a1ee65933c2b
yogesh-nag <144353469+yogesh-nag@users.noreply.github.com>
2024-03-07 17:45:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// (3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3)  // code readablity should be high
+
+console.log("yogesh")
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standelone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined) //undefined
