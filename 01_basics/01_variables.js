1b84e6390e180f74864fc9c508578b2fcd76fc6d
yogesh-nag <144353469+yogesh-nag@users.noreply.github.com>
2024-03-07 17:08:12 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14459
+let accountemail = "yogesh@google.com"
+var accountpassword = "2345"
+accountcity = "Raipur"
+
+
+
+accountemail = "hhg@google.com"
+accountpassword = "6574"
+accountcity = "mumbai"
+
+console.table([accountId,accountemail,accountpassword,accountcity])
