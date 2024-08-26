e1692ca46e49c340be13ca8601d084ec8470afa2
yogesh-nag <144353469+yogesh-nag@users.noreply.github.com>
2024-08-26 22:09:52 +0000

    conversion and operation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,26 +1,56 @@
-let age = "33"
+// let age = "33"
 
-console.log(typeof age);
-console.log(typeof(age));
+// console.log(typeof age);
+// console.log(typeof(age));
 
-let valueInNumber = Number(age)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NAN
 // true => 1; false => 0
 
-let isLoggedIn = undefined
+// let isLoggedIn = undefined
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true ; 0 => false;
 // "yogesh" => true
 
-let someNumber = 32
+// let someNumber = 32
 
-let StringNumber = String(someNumber)
-console.log(StringNumber)
-console.log(typeof(StringNumber))
+// let StringNumber = String(someNumber)
+// console.log(StringNumber)
+// console.log(typeof(StringNumber))
+
+                 //*****************Operations************************* *//
+
+
+// let value = 5
+// let Negvalue = -value
+// console.log(Negvalue)
+
+// console.log(2-2)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+// let str1 = "yogesh"
+// let str2 = " nag"
+// console.log(str1 + str2)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(true)
+// console.log(+true)
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
