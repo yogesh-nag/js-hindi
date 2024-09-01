a6671c3287988ae7c55d3829910275f1a893939e
yogesh-nag <144353469+yogesh-nag@users.noreply.github.com>
2024-09-01 23:41:19 +0000

    num and maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,37 @@
+const number = 100
+// console.log(number);
+
+const balance = new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(1));
+
+const othernumber = 123.8966
+
+// console.log(othernumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4, 3, 7, 9));
+// console.log(Math.max(4, 5, 7, 8));
+
+
+
+console.log(Math.random());
+console.log((Math.random()*10));
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1) + min));
+
