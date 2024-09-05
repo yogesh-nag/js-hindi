2f1de3415ade105141dab962c98d1c4e7dce9aeb
yogesh-nag <144353469+yogesh-nag@users.noreply.github.com>
2024-09-05 17:40:08 +0000

    Dates

---
+++ 01_basics/07_Datesinjs.js
@@ -0,0 +1,24 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(typeof mydate);
+// let myCreatedate = new Date(2024, 10, 15,);
+// let myCreatedate = new Date(2024, 10, 15, 5, 3);
+let myCreatedate = new Date("2024-10-15");
+// console.log(myCreatedate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
