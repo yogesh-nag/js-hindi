a52c73e499ee34811d02b62dfbd4771502e42a05
yogesh-nag <144353469+yogesh-nag@users.noreply.github.com>
2024-09-06 04:10:57 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes = ["Ironman", "spiderman", "batman", "antman"]
+// console.log(myArr[0]);
+// console.log(myHeroes);
+
+const myArr2 = new Array( 1, 2, 3, 4, 5, 6,)
+// console.log(myArr2);
+// myArr.push(6);
+// console.log(myArr);
+// myArr2.pop()
+// console.log(myArr2);
+// myArr.unshift(5)
+// myArr.shift()
+// console.log(myArr.includes(3));
+// console.log(myArr.indexOf(9));
+
+// const newArray = myArr.join()
+// console.log(myArr);
+// console.log(newArray);
+
+// Slice , Splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1 , 3)
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1 , 3)
+console.log("c",myArr);
+console.log(myn2);
